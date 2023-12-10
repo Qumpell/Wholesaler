c60de6165ffdd40a438a022bee552d6df7c8f38c
Qumpell <qqumpel@gmail.com>
2023-12-10 02:00:46 +0100

    Add unique constraint for name of role and exception handler for dataIntegrityViolationException

---
+++ src/main/java/pl/matkan/wholesaler/exception/ExceptionHandlerController.java
@@ -0,0 +1,17 @@
+package pl.matkan.wholesaler.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Unique constraint violation: " + ex.getMessage());
+    }
+}
+
